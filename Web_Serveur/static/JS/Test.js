28de28ff2e60cd33a7b3174d4d0441118968f4f1
Yannouille29 <119313200+Yannouille29@users.noreply.github.com>
2023-11-20 17:44:52 +0000

    Update Test.js
    comment

--- Web_Serveur/static/JS/Test.js
+++ Web_Serveur/static/JS/Test.js
@@ -1,111 +1,108 @@
 function extractAltitude(response) {
-    // Vérifiez si la réponse a des données
+    // Check if the response has data and contains enough columns and rows
     if (response.data.length > 0 && response.data[0].length > 35) {
-        // Assurez-vous qu'il y a au moins une ligne et 11 colonnes dans la première ligne (0-10)
-        var altitude = response.data[2][28]; // La température est dans la 11e colonne de la première ligne
-        // console.log("Température extraite : " + temperature);
-        // Appelez la fonction pour mettre à jour la température dans l'animation
+        // Ensure there is at least one row and 36 columns in the first row (0-35)
+        var altitude = response.data[2][28]; // Altitude is in the 29th column of the third row (index 28)
+        // Call the function to update the altitude in the animation
         updateAltitude(altitude);
     } else {
-        console.error("La réponse ne contient pas suffisamment de lignes ou de colonnes pour extraire la température.");
+        console.error("The response does not contain enough rows or columns to extract altitude.");
     }
 }
 
 function updateAltitude(alt) {
-    alt = Math.max(0, Math.min(2500, alt));
-    var montHeight = (alt / 2500) * 100 + '%';
-    $('#mont').css('height', montHeight);
+    alt = Math.max(0, Math.min(2500, alt)); // Limit altitude between 0 and 2500
+    var montHeight = (alt / 2500) * 100 + '%'; // Calculate height percentage based on altitude
+    $('#mont').css('height', montHeight); // Update the height of the element with ID 'mont' (assumed to represent altitude)
 }
 
-
 function extractPressure(response) {
-    // Vérifiez si la réponse a des données
+    // Check if the response has data and contains enough columns and rows
     if (response.data.length > 0 && response.data[0].length > 35) {
-        // Assurez-vous qu'il y a au moins une ligne et 11 colonnes dans la première ligne (0-10)
-        var pressure = response.data[2][27]; // La température est dans la 11e colonne de la première ligne
-        // console.log("Température extraite : " + temperature);
-        // Appelez la fonction pour mettre à jour la température dans l'animation
+        // Ensure there is at least one row and 36 columns in the first row (0-35)
+        var pressure = response.data[2][27]; // Pressure is in the 28th column of the third row (index 27)
+        // Call the function to update the arrow position in the animation
         updateArrowPosition(pressure);
     } else {
-        console.error("La réponse ne contient pas suffisamment de lignes ou de colonnes pour extraire la température.");
+        console.error("The response does not contain enough rows or columns to extract pressure.");
     }
 }
 
 function updateArrowPosition(press) {
-    // Limitez le pourcentage entre 0 et 100
+    // Limit the pressure value between 0 and 2000
     press = Math.max(0, Math.min(2000, press));
-    // Convertissez le pourcentage en un angle entre -90 et 90 degrés
+    // Convert the pressure value to an angle between -90 and 90 degrees
     var angle = (press / 2000) * 180 - 90;
-    // Mettez à jour la rotation de la flèche
+    // Update the rotation of the arrow
     document.getElementById('arrow').style.transform = 'rotate(' + angle + 'deg)';
 }
 
 function extractHumidity(response) {
-    // Vérifiez si la réponse a des données
+    // Check if the response has data and contains enough columns and rows
     if (response.data.length > 0 && response.data[0].length > 35) {
-        // Assurez-vous qu'il y a au moins une ligne et 11 colonnes dans la première ligne (0-10)
-        var humidity = response.data[2][35]; // La température est dans la 11e colonne de la première ligne
-        // console.log("Température extraite : " + temperature);
-        // Appelez la fonction pour mettre à jour la température dans l'animation
+        // Ensure there is at least one row and 36 columns in the first row (0-35)
+        var humidity = response.data[2][35]; // Humidity is in the 36th column of the third row (index 35)
+        // Call the function to update the humidity in the animation
         updateHumidity(humidity);
     } else {
-        console.error("La réponse ne contient pas suffisamment de lignes ou de colonnes pour extraire la température.");
+        console.error("The response does not contain enough rows or columns to extract humidity.");
     }
 }
 
 function updateHumidity(hum) {
-    hum = Math.max(0, Math.min(800, hum));
-    var waterHeight = (hum / 800) * 100 + '%';
-    $('#water').css('height', waterHeight);
+    hum = Math.max(0, Math.min(800, hum)); // Limit humidity between 0 and 800
+    var waterHeight = (hum / 800) * 100 + '%'; // Calculate height percentage based on humidity
+    $('#water').css('height', waterHeight); // Update the height of the element with ID 'water' (assumed to represent humidity)
 }
 
 function extractTemperature(response) {
-    // Vérifiez si la réponse a des données
+    // Check if the response has data and contains enough columns and rows
     if (response.data.length > 0 && response.data[0].length > 10) {
-        // Assurez-vous qu'il y a au moins une ligne et 11 colonnes dans la première ligne (0-10)
-        var temperature = response.data[2][12] / 10; // La température est dans la 11e colonne de la première ligne
-        // console.log("Température extraite : " + temperature);
-        // Appelez la fonction pour mettre à jour la température dans l'animation
+        // Ensure there is at least one row and 11 columns in the first row (0-10)
+        var temperature = response.data[2][12] / 10; // Temperature is in the 13th column of the third row (index 12)
+        // Call the function to update the temperature in the animation
         updateTemperature(temperature);
     } else {
-        console.error("La réponse ne contient pas suffisamment de lignes ou de colonnes pour extraire la température.");
+        console.error("The response does not contain enough rows or columns to extract the temperature.");
     }
 }
 
 function updateTemperature(temp) {
-    temp = Math.max(0, Math.min(50, temp));
-    var mercuryHeight = (temp / 50) * 100 + '%';
-    $('#mercury').css('height', mercuryHeight);
+    temp = Math.max(0, Math.min(50, temp)); // Limit temperature between 0 and 50
+    var mercuryHeight = (temp / 50) * 100 + '%'; // Calculate height percentage based on temperature
+    $('#mercury').css('height', mercuryHeight); // Update the height of the element with ID 'mercury' (presumably representing temperature)
 }
 
 function extractBatterie(response) {
-    // Vérifiez si la réponse a des données
+    // Check if the response has data and contains enough columns and rows
     if (response.data.length > 0 && response.data[0].length > 10) {
-        // Assurez-vous qu'il y a au moins une ligne et 11 colonnes dans la première ligne (0-10)
-        var batterie = response.data[0][7] / 100; // La température est dans la 7e colonne de la première ligne
-        console.log("bat extraite : " + batterie);
-        // Appelez la fonction pour mettre à jour la température dans l'animation
+        // Ensure there is at least one row and 11 columns in the first row (0-10)
+        var batterie = response.data[0][7] / 100; // Battery level is in the 8th column of the first row (index 7)
+        console.log("Battery extracted : " + batterie);
+        // Call the function to update the battery level in the animation
         updateBatterie(batterie);
     } else {
-        console.error("La réponse ne contient pas suffisamment de lignes ou de colonnes pour extraire la température.");
+        console.error("The response does not contain enough rows or columns to extract the battery level.");
     }
 }
-
+// Function to update the battery level visualization
 function updateBatterie(bat) {
-    bat = Math.max(0, Math.min(5, bat));
-    var batterieHeight = (bat / 5) * 100 + '%';
-    $('#batterieplein').css('height', batterieHeight);
+    bat = Math.max(0, Math.min(5, bat)); // Limit battery level between 0 and 5
+    var batterieHeight = (bat / 5) * 100 + '%'; // Calculate height percentage based on battery level
+    $('#batterieplein').css('height', batterieHeight); // Update the height of the element with ID 'batterieplein' (representing battery level)
 }
 
+// Function to fetch data from the backend (PC)
 function getData() {
-    // Faites une requête pour récupérer les données du back-end (le PC)
+    // Make a request to fetch data from the backend (PC)
     axios.get("http://localhost:8080/get_data")
         .then(function (response) {
-            // Créez le tableau en JavaScript
+            // Create a table in JavaScript
             var table = document.getElementById('data-table');
             var tbody = table.getElementsByTagName('tbody')[0];
-            tbody.innerHTML = '';  // Effacez le contenu actuel du tableau
+            tbody.innerHTML = ''; // Clear the current table content
             console.log(response);
+            // Populate the table with received data
             response.data.forEach(function (rowArray) {
                 var newRow = tbody.insertRow(tbody.rows.length);
 
@@ -115,6 +112,7 @@ function getData() {
                 });
             });
             document.getElementById('arrow').style.transform = 'rotate(0deg)';
+            // Extract and update various data points from the response
             extractTemperature(response);
             extractBatterie(response);
             extractHumidity(response);
@@ -122,12 +120,13 @@ function getData() {
             extractAltitude(response);
         })
         .catch(function (error) {
-            console.error("Erreur lors de la récupération des données : " + error);
+            console.error("Error fetching data: " + error);
         });
 }
 
-// Actualisez les données toutes les secondes
+// Refresh data every second
 setInterval(getData, 1000);
 
-// Appelez getData une première fois pour afficher les données initiales
+// Call getData initially to display initial data
 getData();
+
