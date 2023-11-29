452a4a9b1c7dd26298ee12d65f10cfe827d5ef54
Yannouille29 <119313200+Yannouille29@users.noreply.github.com>
2023-11-29 18:44:58 +0000

    Add files via upload

--- Web_Serveur/static/JS/Test.js
+++ Web_Serveur/static/JS/Test.js
@@ -1,66 +1,70 @@
 function extractAltitude(response) {
-    // Check if the response has data and contains enough columns and rows
+    // Check if the response has data
     if (response.data.length > 0 && response.data[0].length > 35) {
-        // Ensure there is at least one row and 36 columns in the first row (0-35)
-        var altitude = response.data[2][28]; // Altitude is in the 29th column of the third row (index 28)
-        // Call the function to update the altitude in the animation
+        // Ensure there is at least one row and 11 columns in the first row (0-10)
+        var altitude = response.data[2][28]; // Altitude is in the 11th column of the first row
+        // console.log("Extracted Altitude: " + altitude);
+        // Call function to update the altitude in the animation
         updateAltitude(altitude);
     } else {
-        console.error("The response does not contain enough rows or columns to extract altitude.");
+        console.error("The response does not contain enough rows or columns to extract the altitude.");
     }
 }
 
 function updateAltitude(alt) {
-    alt = Math.max(0, Math.min(2500, alt)); // Limit altitude between 0 and 2500
-    var montHeight = (alt / 2500) * 100 + '%'; // Calculate height percentage based on altitude
-    $('#mont').css('height', montHeight); // Update the height of the element with ID 'mont' (assumed to represent altitude)
+    alt = Math.max(0, Math.min(2500, alt));
+    var montHeight = (alt / 2500) * 100 + '%';
+    $('#mont').css('height', montHeight);
 }
 
 function extractPressure(response) {
-    // Check if the response has data and contains enough columns and rows
+    // Check if the response has data
     if (response.data.length > 0 && response.data[0].length > 35) {
-        // Ensure there is at least one row and 36 columns in the first row (0-35)
-        var pressure = response.data[2][27]; // Pressure is in the 28th column of the third row (index 27)
-        // Call the function to update the arrow position in the animation
+        // Ensure there is at least one row and 11 columns in the first row (0-10)
+        var pressure = response.data[2][27]; // Pressure is in the 10th column of the first row
+        // console.log("Extracted Pressure: " + pressure);
+        // Call function to update the arrow position in the animation
         updateArrowPosition(pressure);
     } else {
-        console.error("The response does not contain enough rows or columns to extract pressure.");
+        console.error("The response does not contain enough rows or columns to extract the pressure.");
     }
 }
 
 function updateArrowPosition(press) {
-    // Limit the pressure value between 0 and 2000
+    // Limit the pressure between 0 and 2000
     press = Math.max(0, Math.min(2000, press));
-    // Convert the pressure value to an angle between -90 and 90 degrees
+    // Convert the pressure percentage to an angle between -90 and 90 degrees
     var angle = (press / 2000) * 180 - 90;
     // Update the rotation of the arrow
     document.getElementById('arrow').style.transform = 'rotate(' + angle + 'deg)';
 }
 
 function extractHumidity(response) {
-    // Check if the response has data and contains enough columns and rows
+    // Check if the response has data
     if (response.data.length > 0 && response.data[0].length > 35) {
         // Ensure there is at least one row and 36 columns in the first row (0-35)
-        var humidity = response.data[2][35]; // Humidity is in the 36th column of the third row (index 35)
-        // Call the function to update the humidity in the animation
+        var humidity = response.data[2][35]; // Humidity is in the 36th column of the first row
+        // console.log("Extracted Humidity: " + humidity);
+        // Call function to update the humidity in the animation
         updateHumidity(humidity);
     } else {
-        console.error("The response does not contain enough rows or columns to extract humidity.");
+        console.error("The response does not contain enough rows or columns to extract the humidity.");
     }
 }
 
 function updateHumidity(hum) {
-    hum = Math.max(0, Math.min(800, hum)); // Limit humidity between 0 and 800
-    var waterHeight = (hum / 800) * 100 + '%'; // Calculate height percentage based on humidity
-    $('#water').css('height', waterHeight); // Update the height of the element with ID 'water' (assumed to represent humidity)
+    hum = Math.max(0, Math.min(800, hum));
+    var waterHeight = (hum / 800) * 100 + '%';
+    $('#water').css('height', waterHeight);
 }
 
 function extractTemperature(response) {
-    // Check if the response has data and contains enough columns and rows
+    // Check if the response has data
     if (response.data.length > 0 && response.data[0].length > 10) {
-        // Ensure there is at least one row and 11 columns in the first row (0-10)
-        var temperature = response.data[2][12] / 10; // Temperature is in the 13th column of the third row (index 12)
-        // Call the function to update the temperature in the animation
+        // Ensure there is at least one row and 13 columns in the first row (0-12)
+        var temperature = response.data[2][12] / 10; // Temperature is in the 13th column of the first row
+        // console.log("Extracted Temperature: " + temperature);
+        // Call function to update the temperature in the animation
         updateTemperature(temperature);
     } else {
         console.error("The response does not contain enough rows or columns to extract the temperature.");
@@ -68,41 +72,39 @@ function extractTemperature(response) {
 }
 
 function updateTemperature(temp) {
-    temp = Math.max(0, Math.min(50, temp)); // Limit temperature between 0 and 50
-    var mercuryHeight = (temp / 50) * 100 + '%'; // Calculate height percentage based on temperature
-    $('#mercury').css('height', mercuryHeight); // Update the height of the element with ID 'mercury' (presumably representing temperature)
+    temp = Math.max(0, Math.min(50, temp));
+    var mercuryHeight = (temp / 50) * 100 + '%';
+    $('#mercury').css('height', mercuryHeight);
 }
 
-function extractBatterie(response) {
-    // Check if the response has data and contains enough columns and rows
+function extractBattery(response) {
+    // Check if the response has data
     if (response.data.length > 0 && response.data[0].length > 10) {
         // Ensure there is at least one row and 11 columns in the first row (0-10)
-        var batterie = response.data[0][7] / 100; // Battery level is in the 8th column of the first row (index 7)
-        console.log("Battery extracted : " + batterie);
-        // Call the function to update the battery level in the animation
-        updateBatterie(batterie);
+        var battery = response.data[0][7] / 100; // Battery level is in the 8th column of the first row
+        console.log("Extracted battery level: " + battery);
+        // Call function to update the battery level in the animation
+        updateBattery(battery);
     } else {
-        console.error("The response does not contain enough rows or columns to extract the battery level.");
+        console.error("The response does not contain enough rows or columns to extract the temperature.");
     }
 }
-// Function to update the battery level visualization
-function updateBatterie(bat) {
-    bat = Math.max(0, Math.min(5, bat)); // Limit battery level between 0 and 5
-    var batterieHeight = (bat / 5) * 100 + '%'; // Calculate height percentage based on battery level
-    $('#batterieplein').css('height', batterieHeight); // Update the height of the element with ID 'batterieplein' (representing battery level)
+
+function updateBattery(bat) {
+    bat = Math.max(0, Math.min(5, bat));
+    var batteryHeight = (bat / 5) * 100 + '%';
+    $('#batterieplein').css('height', batteryHeight);
 }
 
-// Function to fetch data from the backend (PC)
 function getData() {
     // Make a request to fetch data from the backend (PC)
     axios.get("http://localhost:8080/get_data")
         .then(function (response) {
-            // Create a table in JavaScript
+            // Create the JavaScript table
             var table = document.getElementById('data-table');
             var tbody = table.getElementsByTagName('tbody')[0];
-            tbody.innerHTML = ''; // Clear the current table content
+            tbody.innerHTML = '';  // Clear the current table content
             console.log(response);
-            // Populate the table with received data
             response.data.forEach(function (rowArray) {
                 var newRow = tbody.insertRow(tbody.rows.length);
 
@@ -111,10 +113,12 @@ function getData() {
                     newCell.innerHTML = cell;
                 });
             });
+            // Reset arrow rotation
             document.getElementById('arrow').style.transform = 'rotate(0deg)';
-            // Extract and update various data points from the response
+            
+            // Extract and update temperature, battery, humidity, pressure, and altitude
             extractTemperature(response);
-            extractBatterie(response);
+            extractBattery(response);
             extractHumidity(response);
             extractPressure(response);
             extractAltitude(response);
