676883a7cf3dd205f591f24a0ef02c28308e590d
Yannouille29 <119313200+Yannouille29@users.noreply.github.com>
2023-11-29 18:50:54 +0000

    Add files via upload

--- Web_Serveur/static/JS/Visu.js
+++ Web_Serveur/static/JS/Visu.js
@@ -1,50 +1,57 @@
+//----------------------------------------------------------------------------------------------------------------
+//-----------------------------Script for the visualization on the web interface----------------------------------
+//----------------------------------Creates by Yann HUGUET in 29/11/2023------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+
+// Target width and height for the animation
 var targetWidth = 1500; // Target width
 var targetHeight = 743.617; // Target height
 
-// Creating a GSAP Timeline to control animations
-// var timeline = gsap.timeline();
-
+// Function to animate the cube rotation based on pitch, yaw, and roll values
 function animateCubeRotation(pitch, yaw, roll) {
-    // If all values (pitch, yaw, roll) are equal to zero, orient the cube with one face towards the viewpoint
+    // If all values (pitch, yaw, roll) are equal to zero, orient the cube with a face towards the viewpoint
     if (pitch === 0 && yaw === 0 && roll === 0) {
         // Change the default orientation of the cube based on the camera view
-        var defaultOrientation = calculateDefaultOrientation();
+        var defaultOrientation = calculateDefaultOrientation(); // Function call to calculate the default orientation
         gsap.to(cube.rotation, {
             x: defaultOrientation.x,
             y: defaultOrientation.y,
             z: defaultOrientation.z,
-            duration: 0.3,
-            ease: "power2.inOut"
+            duration: 0.3, // Duration of the animation
+            ease: "power2.inOut" // Easing function for animation
         });
     } else {
-        // Rotate the cube to the provided pitch, yaw, and roll angles
+        // Rotate the cube based on provided pitch, yaw, and roll values
         gsap.to(cube.rotation, {
             x: pitch,
             y: yaw,
             z: roll,
-            duration: 0.3,
-            ease: "power2.inOut"
+            duration: 0.3, // Duration of the animation
+            ease: "power2.inOut" // Easing function for animation
         });
     }
 }
+
 // Function to calculate the default orientation when pitch, yaw, and roll are all zero
 function calculateDefaultOrientation() {
     // Determine the default orientation based on the camera view
     // For example, facing forward (positive z)
-    // You can adjust these values according to the desired orientation
-    var defaultX = 0;  // Adjust these values to orient the cube along the X axis
-    var defaultY = 0;  // Adjust these values to orient the cube along the Y axis
-    var defaultZ = 0;  // Adjust these values to orient the cube along the Z axis
+    // You can adjust these values to orient the cube along the X, Y, Z axes as desired
+    var defaultX = 0;  // Adjust these values to orient the cube along the X-axis
+    var defaultY = 0;  // Adjust these values to orient the cube along the Y-axis
+    var defaultZ = 0;  // Adjust these values to orient the cube along the Z-axis
 
     return { x: defaultX, y: defaultY, z: defaultZ };
 }
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, targetWidth / targetHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-var canMakeRequest = true;
+// Creating a Three.js scene, camera, and renderer
+var scene = new THREE.Scene();  // Creating a Three.js scene
+var camera = new THREE.PerspectiveCamera(75, targetWidth / targetHeight, 0.1, 1000);  // Creating a camera
+var renderer = new THREE.WebGLRenderer();  // Creating a WebGLRenderer
 
-// Adding four spotlights distributed in the corners of the scene
+var canMakeRequest = true;  // Variable to control if a request can be made
+
+// Adding four spot lights positioned at different corners of the scene
 const spotLight1 = new THREE.SpotLight(0xffffff); // White light
 spotLight1.position.set(-20, 20, -20);
 scene.add(spotLight1);
@@ -57,107 +64,102 @@ const spotLight3 = new THREE.SpotLight(0xffffff); // White light
 spotLight3.position.set(-20, 20, 20);
 scene.add(spotLight3);
 
-const spotLight4 = new THREE.SpotLight(0xffffff); // Lumière blanche
+const spotLight4 = new THREE.SpotLight(0xffffff); // White light
 spotLight4.position.set(20, 20, 20);
 scene.add(spotLight4);
 
-const ambientLight = new THREE.AmbientLight(0xffc0c0); // Couleur rouge plus claire pour la lumière ambiante
+const ambientLight = new THREE.AmbientLight(0xffc0c0); // Light with a lighter shade of red for ambient lighting
 scene.add(ambientLight);
 
-// Création d'un cube
-var geometry = new THREE.BoxGeometry(3, 3, 3);
+// Creating a cube
+var geometry = new THREE.BoxGeometry(3, 3, 3); // Box geometry with dimensions 3x3x3
 
 var textureLoader = new THREE.TextureLoader();
 
+// Loading textures and creating materials for each face of the cube
 var materials = Object.values(imageUrls).map(url => {
     var texture = textureLoader.load(url);
     return new THREE.MeshStandardMaterial({ map: texture });
 });
 
+// Creating a mesh (cube) by combining geometry and materials
 var cube = new THREE.Mesh(geometry, materials);
 
 // Camera position
-camera.position.z = 5;
+camera.position.z = 5; // Set the camera position along the z-axis
 
 // Adding the cube to the scene
 scene.add(cube);
 
 // Create an AxesHelper and attach it to the cube
-const axesHelper = new THREE.AxesHelper(5);
-cube.add(axesHelper);
+const axesHelper = new THREE.AxesHelper(5); // AxesHelper with a size of 5 units
+cube.add(axesHelper); // Attach the AxesHelper to the cube
 
-// Position the AxesHelper to be aligned with the cube (at the center, for example)
-axesHelper.position.set(0, 0, 0);
+// Position the AxesHelper to align with the cube (for example, at the center)
+axesHelper.position.set(0, 0, 0); // Set the position of the AxesHelper relative to the cube
 
-// Function to resize the canvas based on the window dimensions
+// Function to resize the canvas
 function resizeCanvas() {
     var newWidth = window.innerWidth;
 
-    // Ensure the canvas width doesn't exceed the target width
+    // Limit the newWidth to the targetWidth
     if (newWidth > targetWidth) {
         newWidth = targetWidth;
     }
 
-    var newHeight = (newWidth / targetWidth) * targetHeight;
+    var newHeight = (newWidth / targetWidth) * targetHeight; // Calculate new height based on aspect ratio
 
-    // Set renderer size and update camera aspect ratio
+    // Set renderer size, camera aspect ratio, and update projection matrix
     renderer.setSize(newWidth, newHeight);
     camera.aspect = newWidth / newHeight;
     camera.updateProjectionMatrix();
 }
 
-// Add event listeners to handle window load and resize events
-window.addEventListener('load', resizeCanvas);
-window.addEventListener('resize', resizeCanvas);
+// Event listeners for window load and resize events
+window.addEventListener('load', resizeCanvas); // Call resizeCanvas when the window loads
+window.addEventListener('resize', resizeCanvas); // Call resizeCanvas when the window is resized
 
-// Resize the canvas on page load
-resizeCanvas();
+resizeCanvas(); // Initial call to resizeCanvas function
 
-// Append the renderer's DOM element to the visualization container in the HTML
+// Append the renderer's DOM element to the visualization container
 document.getElementById("visualization-container").appendChild(renderer.domElement);
-
-// Function to perform animation and retrieve data
 function animate() {
     if (canMakeRequest) {
-        canMakeRequest = false;
+        canMakeRequest = false; // Prevent making another request until the current one is finished
 
-        // Make an HTTP GET request using axios to retrieve data from the server
-        axios.get("http://localhost:8080/get_data")
+        axios.get("http://localhost:8080/get_data") // Make a GET request to fetch data from a local server
             .then(function (response) {
                 if (response.data.length > 0) {
-                    var latestData = response.data[0]; // Retrieve the first array of data
+                    var latestData = response.data[0]; // Extracting the first array of data
 
-                    // Check if the data array has enough values
                     if (latestData.length >= 34) {
-                        // Extract roll, pitch, and yaw values from the received data
+                        // Extract roll, pitch, and yaw values from the data array and adjust them
                         var roll = parseFloat(latestData[31]) - 2048; // 32nd value (index 31)
                         var pitch = parseFloat(latestData[32]) - 2048; // 33rd value (index 32)
                         var yaw = parseFloat(latestData[33]) - 2048; // 34th value (index 33)
 
-                        // Convert roll, pitch, and yaw to radians
+                        // Convert roll, pitch, and yaw values to radians
                         roll = (Math.PI / 180) * roll;
                         pitch = (Math.PI / 180) * pitch;
                         yaw = (Math.PI / 180) * yaw;
 
-                        // Call the animateCubeRotation function to animate the rotation based on the received data
-                        animateCubeRotation(pitch, yaw, roll);
+                        animateCubeRotation(pitch, yaw, roll); // Use the updated function to animate rotation
                     }
                 }
 
-                // Render the scene with updated changes
-                renderer.render(scene, camera);
+                renderer.render(scene, camera); // Render the scene with the updated rotation
             })
             .catch(function (error) {
                 console.error("Error fetching data: " + error);
             })
             .finally(function () {
-                canMakeRequest = true;
+                canMakeRequest = true; // Allow making a new request after finishing the current one
             });
     }
 }
 
-// Call the animate function every 10 milliseconds using setInterval
+// Call the 'animate' function every 10 milliseconds using setInterval
 setInterval(animate, 10);
 
-// Initial call to the animate function
+// Call 'animate' initially to start the animation loop
 animate();
